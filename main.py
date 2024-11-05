74682290de65a06ff822db04e38ada0e025d72ef
Jalen Henderson <jkhenderson@loyola.edu>
2024-11-05 16:24:05 -0500

    Code

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import random
 sum_list = [] * 12
 amount_of_rolls = int(input("How many rolls would you like to calculate?"))
 count = 0
+
 while count < amount_of_rolls:
     total = random.randint(2,12)
     sum_list.append(total)
