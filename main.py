73fb67e36c60aeeb9e9fe8a41e02f1c207f12146
theresadejacimo <tadejacimo@loyola.edu>
2024-11-06 22:41:42 -0500

    start

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import random
 
 
+import random
 
-sum_list = [] * 12
+
+
+sum_list = [0] * 13
 amount_of_rolls = int(input("How many rolls would you like to calculate?"))
 count = 0
 
 while count < amount_of_rolls:
     total = random.randint(2,12)
-    sum_list.append(total)
+    sum_list[total] += 1
     count += 1
 
 
-for i in (sum_list):
+for i in (range(2,13)):
         print("Sum of", [i], sum_list[i] * "*")
